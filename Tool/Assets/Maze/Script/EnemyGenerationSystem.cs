93565253bc8c5adef7d11015a68f381d48bdde35
ta45668 <55038601+ta45668@users.noreply.github.com>
2020-07-09 11:43:51 +0800

    完成生怪系統
    優化迷宮
    接下來做人物系統(玩家)

---
+++ Tool/Assets/Maze/Script/EnemyGenerationSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerationSystem : MonoBehaviour
+{
+    #region 敵人生成系統宣告
+    [Header("敵人物件清單"),SerializeField]
+    private List<Transform> enemyObj = new List<Transform>();
+    [Header("敵人數量調整"), Range(1.0f, 4.0f)]
+    public float enemyQuantity = 1.0f;
+    [Header("敵人生成空間"), SerializeField]
+    private Transform enemySpace;
+
+    Transform enemyScratchpad;//敵人暫存器
+    #endregion
+    /// <summary>
+    /// 初始化(initialization)這支程式的方法
+    /// </summary>
+    public void Initial()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /// <summary>
+    /// 決定敵人物件的方法
+    /// </summary>
+    void DecideEnemyObj()
+    {
+        int number = Random.Range(0, enemyObj.Count);
+        enemyScratchpad = enemyObj[number];
+    }
+    /// <summary>
+    /// 敵人生成器的方法
+    /// </summary>
+    /// <param name="place">生成地點</param>
+    /// <param name="number">生成編號</param>
+    public void EnemyBuilder(Vector3 place,int number)
+    {
+        place += Vector3.up;//矯正生成位置
+        DecideEnemyObj();//確認生成物
+        Transform enemy = Instantiate(enemyScratchpad, place, Quaternion.identity, enemySpace);//生成
+        enemy.name = enemyScratchpad.name + "(" + number.ToString() + ")";//改名子
+    }
+}
--- Tool/Assets/Maze/Script/LabyrinthSystem.cs
+++ Tool/Assets/Maze/Script/LabyrinthSystem.cs
@@ -4,29 +4,43 @@ using UnityEngine;
 
 public class LabyrinthSystem : MonoBehaviour
 {
-    #region 迷宮系統宣告space
+    #region 迷宮系統宣告
     [Header("迷宮大小(X*X)")]
     public int labyrinthSize;
+    [Header("迷宮組成物件"), Tooltip("0=地板，1=牆壁Z，2=牆壁Y"), SerializeField]
+    private GameObject[] labyrinthCube = new GameObject[3];
+    /*舊宣告物件
     [Header("地板物件")]
     public GameObject floorCube;
     [Header("牆壁物件(Z)")]
     public GameObject wallCubeZ;
     [Header("牆壁物件(Y)")]
     public GameObject wallCubeY;
+    */
     [Header("牆壁生成空間"), SerializeField]
     private Transform wallSpace;
+    [Header("地板生成空間"), SerializeField]
+    private Transform floorSpace;
     [Header("牆壁高度")]
     public int wallHigh;
-    
-    List<Transform> floorList = new List<Transform>();//地板清單(用來控制用)
+    [HideInInspector]//在面板中隱藏資料
+    public List<Transform> floorList = new List<Transform>();//地板清單(用來控制用)
+    /// <summary>
+    /// 宣告enemyGenerationSystem是屬於EnemyGenerationSystem程式碼的代理人
+    /// </summary>
+    EnemyGenerationSystem enemyGenerationSystem
+    {
+        get { return GameObject.Find("EnemyGeneration").GetComponent<EnemyGenerationSystem>(); }//取得在此物件上的程式碼
+    }
     #endregion
     // Start is called before the first frame update
     void Start()
     {
+        labyrinthSize = Mathf.Clamp(labyrinthSize, 5, 20);//限制地圖大小[Clamp(你所要判斷的值,最小值,最大值)=讓值在最大最小值之間]
         BuildFloor();//建造地板
         BuildWall(-0.55f);//建造1/2牆
         BuildWall(-0.55f + labyrinthSize);//建造1/2牆
-        BuildPillar();
+        BuildPillarAndEnemy();//建造柱子
     }
 
     // Update is called once per frame
@@ -44,7 +58,7 @@ public class LabyrinthSystem : MonoBehaviour
             for (int j = 0; j < labyrinthSize; j++)
             {
                 Vector3 vector = new Vector3((float)i, 0, (float)j);//指定生成位置
-                GameObject floor = Instantiate(floorCube, vector, Quaternion.identity, transform);//生成地板
+                GameObject floor = Instantiate(labyrinthCube[0], vector, Quaternion.identity, floorSpace);//生成地板
                 floor.name = "FloorCube(" + ((labyrinthSize * i) + j).ToString() + ")";//改名子
                 floorList.Add(floor.transform);//加入清單中(方便管理)
             }
@@ -65,8 +79,8 @@ public class LabyrinthSystem : MonoBehaviour
                 string nameZ = "wallCubeZ(" + ((wallHigh * i) + j).ToString() + ")" + SP.ToString();//命名(Z牆)
                 string nameY = "wallCubeY(" + ((wallHigh * i) + j).ToString() + ")" + SP.ToString();//命名(Y牆)
                 //使用輔助器建造
-                BuildWallSUP(wallCubeZ, vectorZ, nameZ);
-                BuildWallSUP(wallCubeY, vectorY, nameY);
+                BuildWallSUP(labyrinthCube[1], vectorZ, nameZ);//創建牆壁Z
+                BuildWallSUP(labyrinthCube[2], vectorY, nameY);//創建牆壁Y
             }
         }
     }
@@ -82,25 +96,26 @@ public class LabyrinthSystem : MonoBehaviour
         wall.name = wallName;//改名子
     }
     /// <summary>
-    /// 創建柱子的方法
+    /// 創建柱子與敵人的方法
     /// </summary>
-    void BuildPillar()
+    void BuildPillarAndEnemy()
     {
-        int total = labyrinthSize * labyrinthSize;//計算總共有幾格地板
-        bool[] RandomNumberBool = new bool[total];//防止會有重複的地板產生
+        int floorTotal = labyrinthSize * labyrinthSize;//計算總共有幾格地板
+        bool[] RandomNumberBool = new bool[floorTotal];//防止會有重複的地板產生
 
-        for (int i = 0; i < total; i++)//初始所有地板
+        for (int i = 0; i < floorTotal; i++)//初始所有地板
         {
             RandomNumberBool[i] = false;
         }
 
-        int pillarQuantity = 0;//要形成幾次柱子的變數
+        #region 創造柱子的方法
+        int pillarQuantity = 0;//形成柱子的計數器
 
         while (pillarQuantity < labyrinthSize)//生成柱子的方法
         {
-            int pollar = Random.Range(0, total);//隨機一個數
+            int pollar = Random.Range(0, floorTotal);//隨機一個數
 
-            if (!RandomNumberBool[pollar])//如果被隨機到的地板還沒變成柱子
+            if (!RandomNumberBool[pollar])//如果被隨機到的地板還沒變成柱子或敵人
             {
                 #region 變成柱子
                 floorList[pollar].localScale = Vector3.one + Vector3.up * wallHigh;
@@ -110,5 +125,25 @@ public class LabyrinthSystem : MonoBehaviour
                 pillarQuantity++;//次數+1次
             }
         }
+        #endregion
+
+        #region 創造怪物的方法
+        int enemyQuantity = 0;//形成怪物的計數器
+        float enemyCount = Mathf.Round(labyrinthSize * enemyGenerationSystem.enemyQuantity);//計算要生成幾個敵人
+
+        while (enemyQuantity < enemyCount)//生成怪物的方法
+        {
+            int pollar = Random.Range(0, floorTotal);//隨機一個數
+
+            if (!RandomNumberBool[pollar])//如果被隨機到的地板還沒變成柱子或敵人
+            {
+                #region 生成敵人的方法
+                enemyGenerationSystem.EnemyBuilder(floorList[pollar].position, enemyQuantity);
+                RandomNumberBool[pollar] = true;
+                #endregion
+                enemyQuantity++;//次數+1次
+            }
+        }
+        #endregion
     }
 }
