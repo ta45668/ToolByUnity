b6af768879da7220e6fb296ca8f8ed4432ffa36a
ta45668 <55038601+ta45668@users.noreply.github.com>
2020-07-20 11:44:55 +0800

    攻擊動畫優化
    完成翻滾跟射箭的動畫優化
    解決連續射擊出現的BUG
    完成攻擊攝影機的位置切換

--- Tool/Assets/Player/Script/LongbowAction.cs
+++ Tool/Assets/Player/Script/LongbowAction.cs
@@ -8,15 +8,26 @@ public class LongbowAction : MonoBehaviour
     [Header("玩家動畫控制器"), SerializeField]
     private Animator longbowAnimator;
     [Header("玩家物件"), SerializeField]
-    private Transform longbow;
+    private Transform longbow;   
     [Header("玩家翻滾速度")]
     public float diveSpeed;
 
     AnimatorStateInfo stateInfo;//取得動畫 狀態訊息(stateInfo)
     #endregion
+    #region 攻擊鏡頭切換
+    [Header("攝影機物件(世界)"), SerializeField]
+    private Transform longbowCamera;
+    [Header("攝影機物件(攻擊時)"), SerializeField]
+    private Transform cameraPosition;
+    [Header("攝影機物件(原本)"), SerializeField]
+    private Transform originalCameraPos;
+
+    bool isDive = false;//判斷是否翻滾
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
+        isDive = false;//初始
         longbowAnimator = transform.GetChild(0).GetComponent<Animator>();//自動抓取
         longbow = transform.GetChild(0).GetComponent<Transform>();//自動抓取
     }
@@ -33,26 +44,60 @@ public class LongbowAction : MonoBehaviour
             }
             longbowAnimator.SetBool("Dive", false);//結束後取消
         }
+        
+        if (stateInfo.IsName("Base Layer.AimWalkForward"))//如果正在做舉弓動畫
+        {
+            if (!longbowAnimator.IsInTransition(0))//如果此動畫還在播放
+            {
+                longbowCamera.position = cameraPosition.position;//移動位置
+                longbowCamera.rotation = cameraPosition.rotation;//移動旋轉位
+            }
+            longbowAnimator.SetBool("AimWalk", false);//結束後取消
+        }
+
+        if (stateInfo.IsName("Base Layer.AimRecoil"))//如果正在做舉弓動畫
+        {
+            if (!longbowAnimator.IsInTransition(0))//如果此動畫還在播放
+            {
+            }
+            longbowAnimator.SetBool("AimRecoil", false);//結束後取消
+            longbowCamera.position = originalCameraPos.position;//移動位置
+            longbowCamera.rotation = originalCameraPos.rotation;//移動旋轉位
+        }
     }
     /// <summary>
-    /// 翻滾的方法
+    /// 翻滾開始的方法
     /// </summary>
     public void Dive()
     {
         longbowAnimator.SetBool("Dive", true);//開始翻滾動畫
+        isDive = true;
+    }
+    /// <summary>
+    /// 設定翻滾結束的方法
+    /// </summary>
+    public void SetDiveEnd()
+    {
+        isDive = false;
     }
     /// <summary>
     /// 舉起弓箭的方法
     /// </summary>
     public void Aim()
     {
-        longbowAnimator.SetTrigger("AimWalk");
+        if (!isDive)
+        {
+            longbowAnimator.SetBool("AimWalk",true);
+            longbowAnimator.SetBool("AimRecoil", false);
+        }
+        return;
     }
     /// <summary>
     /// 射出箭的方法
     /// </summary>
     public void AimRecoil()
     {
-        longbowAnimator.SetTrigger("AimRecoil");
+        longbowAnimator.SetBool("AimWalk", false);
+        longbowAnimator.SetBool("AimRecoil",true);
     }
 }
---
+++ Tool/Assets/Player/Script/LongbowAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongbowAnimation : MonoBehaviour
+{
+    #region 弓箭手動畫的事件宣告
+    [Header("玩家攻擊物件"),SerializeField]
+    private GameObject playerLongbow;
+    LongbowAction longbowAction;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerLongbow = this.transform.parent.gameObject;
+        longbowAction = playerLongbow.GetComponent<LongbowAction>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /// <summary>
+    /// 啟動翻滾結束的方法(在動畫裡)
+    /// </summary>
+    public void DiveEnd()
+    {
+        longbowAction.SetDiveEnd();
+    }
+
+    
+}
